ef33b7426f91498ebb6dcb84c18584c812b69719
Vitor <vitorcastanhos@gmail.com>
2023-05-08 13:08:23 -0300

    movieguideapp

--- script.js
+++ script.js
@@ -15,33 +15,40 @@ let getMovie = () => {
 
   //if input isnt empty
   else {
-    fetch(url)
-      .then((resp) => resp.json())
-      .then((data) => {
+    fetch(url).then((resp) => resp.json()).then((data) => {
         //if movie exists in database
         if (data.Response == "True") {
           result.innerHTML = `
-        <div class="info">
+
+        <div class="info">        
           <img src=${data.Poster} class="poster">
         <div>
-         <h2>${data.Title}</h2>
+          
+          <h2>${data.Title}</h2>
+
         <div class="rating"> 
-          <img src="./star-icon.svg">
+          <img src="./star.svg">
           <h4>${data.imdbRating}</h4>
         </div>
+
         <div class="details">
           <span>${data.Rated}</span>
           <span>${data.Year}</span>
           <span>${data.Runtime}</span>
         </div>
+
         <div class="genre">
-          <div>${data.Genre.split(",").join("</div><div>")}</div>
-          </div>
+             <div>${data.Genre.split(",").join("</div><div>")}</div>
+           </div>
           </div>
         </div>
+
         <h3>Plot:</h3>
+
         <p>${data.Plot}</p>
+
         <h3>Cast:</h3>
+
         <p>${data.Actors}</p>
         `;
         }
